bae05c4c541b64d3dc5a890ce1149dec5b5f3090
David Vaughan <david@davidv.xyz>
2022-02-01 00:14:19 -0800

    Set up GPG, ssh, password store

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ srcdir="$(pwd)"
 
 mkdir -p $HOME/src
 
-sudo pacman -Sy sed grep awk fzf git artools-base gnupg
+sudo pacman -Sy sed grep awk fzf git artools-base gnupg libssh2 openssh
 
 # Interactively mount drives
 set +x
@@ -52,27 +52,64 @@ sudo fstabgen -U / | sudo tee /etc/fstab
 
 set -euxo pipefail
 
-# Keys and passwords
-# TODO: figure out attaching USBs to VM so you can experiment in real time
-# TODO:
-# ---------------
-# -copy gnupg folder, export pub
-# -delete gnupg
-# -fresh gpg2 init
-# -import the secret + pub
-# -trust the key, ultimately
-# -clone password-store
-# -make a new password
-# -push
-# ---------------
+# GPG keys
 # TODO: pam gnupg setup
-# set up gpg key pair
 gpg --full-gen-key
 
+set +x
+echo "Enter gpg location to import (empty to stop): "
+read secretfile
+while [ "$secretfile" != "" ]; do
+	set -x
+	gpg --import $secretfile
+	set +x
 
-# import private + public key associated with password-store
-# copy passgit private ssh key
-# GIT_SSH_COMMAND="ssh -i ~/.ssh/passgit -F /dev/null" git clone ssh://git@davidv.xyz:/home/git/pass-repo ~/.password-store
+	clear
+	echo "************************************************************************"
+	echo "Edit the gpg key to trush it"
+	echo
+	echo "Hints:"
+	echo "  trust: trust the key"
+	echo "  q: quit"
+	echo
+	echo "Guidelines:"
+	echo "  1. trust"
+	echo "  2. 5 (ultimate)"
+	echo "  3. y (confirm)"
+	echo "  4. q"
+	echo "************************************************************************"
+
+	gpg --edit-key $(gpg --list-keys | fzf --layout=reverse)
+	clear
+
+	echo "Enter gpg location to import (empty to stop): "
+	read secretfile
+done
+
+# SSH keys
+mkdir -p $HOME/.ssh
+echo "Enter ssh key location to copy: "
+read sshfile
+while [ "$sshfile" != "" ]; do
+	cp $sshfile $HOME/.ssh
+
+	echo "Enter ssh key location to copy: "
+	read sshfile
+done
+chmod 600 ~/.ssh/*
+
+# Password store
+GIT_SSH_COMMAND="ssh -i ~/.ssh/passgit -F /dev/null" git clone ssh://git@davidv.xyz:/home/git/pass-repo $HOME/.password-store
+
+# Git config
+echo "Enter git email: "
+read gitemail
+git config --global user.email "$gitemail"
+echo
+
+echo "Enter git name: "
+read gitname
+git config --global user.name "$gitname"
 
 # Set up package settings
 sudo pacman -S artix-archlinux-support
@@ -88,7 +125,7 @@ sudo cp makepkg.conf-sample /etc/makepkg.conf
 sudo pacman -Syu
 
 # Install official packages
-cat pacman-pkgs.txt | sed 's/^#.*//g' | sed '/^$/d' | sudo pacman -Syu -
+cat pacman-pkgs.txt | sed 's/^#.*//g' | sed '/^$/d' | sudo pacman -S -
 
 # Install yay
 sudo pacman -S base-devel
