dcea207c6f1b2182c9b4e3747233e4eaa07aa159
David Vaughan <david@davidv.xyz>
2023-01-20 15:31:14 -0800

    Make a setupreminder on initial login

--- bin/initonce.sh
+++ bin/initonce.sh
@@ -6,6 +6,9 @@ if [ -e "$donefile" ]; then
 fi
 
 # KDE theme
-lookandfeeltool -a org.kde.breezedark.desktop
+lookandfeeltool -a org.kde.breezedark.desktop &
+
+# Setup reminders
+terminal -e sh -c "vim $HOME/.cache/setupreminder" &
 
 touch "$donefile"
